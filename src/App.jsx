beb94ff5ff04d9a8785f2db8aaa8c6dc0bcf4c0e
Merey Kaliyev <yeahmerey@mail.ru>
2025-11-14 09:10:31 +0500

    fetch : base routing logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router";
 import Games from "./components/Games/Games.jsx";
+import Home from "./components/Home/Home.jsx";
+import About from "./components/About/About.jsx";
 function App() {
   return (
-    <div>
-      <Games />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello to Home Page of my project.</h1>
+      <img
+        src="https://cdn.7days.ru/pic/c8d/1001173/1572272/86.jpg"
+        alt="image of Leo"
+      />
+    </div>
+  );
+}
